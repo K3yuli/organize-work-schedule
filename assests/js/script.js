dc1237a328e4ee1235072aa495e67045cc80cc74
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2021-12-05 19:55:14 -0500

    added button aler

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,34 +1,24 @@
+
+
 // the current day is displayed at the top of the calendar
 var dt = new Date();
 document.getElementById("currentDay").innerHTML = (("0" + dt.getDate()).slice(-2)) + "." + (("0" + (dt.getMonth()+1)).slice(-2)) + "." + (dt.getUTCFullYear()) + " " + (("0" + dt.getHours()).slice(-2)) + ":" + (("0"+ dt.getMinutes()).slice(-2));
 
-// when i scroll down
-// then i am presented with time blocks for standard business hrs
-
-
 
 // when i view the time blocks for that day
-// then each time block is color-coded ti indicate whether it is in the
+// then each time block is color-coded to indicate whether it is in the
 // past, present, or future
 
 
-
-// when i click into a time block
-// then i can enter an event
-// $(".description").on("click", "textarea", function() {
-// console.log("<textarea> was clicked");
-// });
-
 // when i click the save button for that time block
 // then the text for that event is saved in local storage
-// $(".saveBtn").on("click", function() {
-//     console.log(this);
-//     var text = $(this).siblings(".description").val();
-//     var time = $(this).parent().attr("id");
-    
-//     localStorage.setItem(time, text);
-    
-// });
+var buttonEl = document.querySelector("#saveButton");
+
+buttonEl.addEventListener("click", function() {
+    console.log("button clicked");
+
+});
+
 
 // when i refresh the page
 // then the saved events persist
